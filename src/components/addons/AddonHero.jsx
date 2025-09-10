65ab610b51cb55d056b1bac186adc77264f10319
Jallox <javiluky2009@gmail.com>
2025-09-10 09:01:15 +0200

    Change Hero message

--- src/components/addons/AddonHero.jsx
+++ src/components/addons/AddonHero.jsx
@@ -4,7 +4,7 @@ function AddonHero() {
     return (
         <div className="relative mx-auto pt-15 bgimg p-5">
             <h1 className='text-4xl text-primary-500 text-center font-extrabold font-code'>Addons</h1>
-            <h2 className='text-xl text-gray-300 text-center'>Increase your 0xbot capabilities with <strong>addons</strong>.</h2>
+            <h2 className='text-xl text-gray-300 text-center'>Increase your 0xbot capabilities with <strong>addons</strong>, also refered as Modules.</h2>
 
             <div className="flex flex-wrap justify-center gap-5">
                 <a href="https://store.smartshub.dev" type="button" className="rounded-lg border border-primary-950 bg-primary-950 px-5 py-2.5 text-center text-sm font-medium text-primary-500 transition-all hover:border-primary-800 hover:bg-primary-800 hover:cursor-pointer focus:ring focus:ring-primary-700 disabled:border-primary-700 disabled:bg-primary-950 disabled:text-primary-400"><i className="bi bi-puzzle"></i> Browse BuiltByBit account</a>
